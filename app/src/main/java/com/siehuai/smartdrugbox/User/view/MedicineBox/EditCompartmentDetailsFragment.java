43523dda6c28d071bfcd1727b9080b1bdf90d32d
siehuai <gans0012@e.ntu.edu.sg>
2017-05-10 15:41:09 +0800

    Ureca 2017: Create EditCompartment Details Fragment

---
+++ app/src/main/java/com/siehuai/smartdrugbox/User/view/MedicineBox/EditCompartmentDetailsFragment.java
@@ -0,0 +1,98 @@
+package com.siehuai.smartdrugbox.User.view.MedicineBox;
+
+
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.siehuai.smartdrugbox.Generic.controller.RemoteDatabaseHelper.RemoteDbFactory;
+import com.siehuai.smartdrugbox.Generic.controller.Service.AlertDialogService;
+import com.siehuai.smartdrugbox.Generic.controller.Service.AlertDialogServiceFactory;
+import com.siehuai.smartdrugbox.R;
+import com.siehuai.smartdrugbox.User.controller.RemoteDatabaseHelper.MedicineBoxCompartmentRemoteHelper;
+import com.siehuai.smartdrugbox.User.data.CompartmentDetails;
+
+public class EditCompartmentDetailsFragment extends Fragment {
+
+    EditText medicineNameEditText;
+    EditText drugStoreEditText;
+    Button confirmBtn;
+    private MedicineBoxCompartmentRemoteHelper mMedicineBoxCompartmentRemoteHelper;
+    private AlertDialogService mAlertDialogService;
+    private String medicineName;
+    private String drugStoreName;
+    CompartmentDetails mCompartmentDetails;
+
+    public EditCompartmentDetailsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle bundle = getArguments();
+        mCompartmentDetails = bundle.getParcelable("compartmentDetails");
+        Log.d("EditCompartmentDetails", String.valueOf(mCompartmentDetails));
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_edit_compartment_details, container, false);
+        medicineNameEditText = (EditText) view.findViewById(R.id.editText_medicineName);
+        drugStoreEditText = (EditText) view.findViewById(R.id.editText_drugStore);
+        confirmBtn = (Button) view.findViewById(R.id.btn_confirm);
+        mMedicineBoxCompartmentRemoteHelper
+                = (MedicineBoxCompartmentRemoteHelper) RemoteDbFactory
+                .createRemoteDbHelper(RemoteDbFactory.RemoteDataType.CompartmentDetails);
+        mAlertDialogService = AlertDialogServiceFactory.createAlertDialogService(getActivity());
+
+        setConfirmBtnOnClick();
+        return view;
+    }
+
+    private void setConfirmBtnOnClick() {
+        confirmBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getAllInputDetails();
+                editCompartmentDetailsRemote();
+            }
+        });
+    }
+
+    private void getAllInputDetails() {
+        medicineName = medicineNameEditText.getText().toString();
+        drugStoreName = drugStoreEditText.getText().toString();
+    }
+
+
+    private void editCompartmentDetailsRemote() {
+        mMedicineBoxCompartmentRemoteHelper.attachOnCompleteListener(new DatabaseReference.CompletionListener() {
+            @Override
+            public void onComplete(DatabaseError databaseError, DatabaseReference databaseReference) {
+                if (databaseError == null) {
+                    mAlertDialogService.provideDefaultOkDialog("Ok,Please Proceed", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    }, null);
+                } else {
+                    mAlertDialogService.provideDefaultErrorDialog(databaseError.toString(), null);
+                }
+            }
+        });
+    }
+
+}
