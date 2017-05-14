c17b89ab5135abffafe47cf66b9fe0b76c651bd6
siehuai <gans0012@e.ntu.edu.sg>
2017-05-14 21:37:27 +0800

    Ureca 2017: Small Changes on DataType

--- app/src/main/java/com/siehuai/smartdrugbox/Generic/data/DataType.java
+++ app/src/main/java/com/siehuai/smartdrugbox/Generic/data/DataType.java
@@ -1,9 +1,9 @@
 package com.siehuai.smartdrugbox.Generic.data;
 
 public class DataType {
-    public static String PharmacyMedicineDetails = "Pharmacy Medicine Details";
-    public static String UserMedicineDetails = "User Medicine Details";
-    public static String MedicineBoxDetails = "Box Details";
-    public static String MedicineBoxCompartmentDetails = "Compartment Details";
-    public static String MedicineBox = "Medicine Box ";
+`    public static String PharmacyMedicineDetails = "Pharmacy-Medicine-Details";
+    public static String UserMedicineDetails = "User-Medicine-Details";
+    public static String MedicineBoxDetails = "Box-Details";
+    public static String MedicineBoxCompartmentDetails = "Compartment-Details";
+    public static String MedicineBox = "Medicine-Box";
 }
