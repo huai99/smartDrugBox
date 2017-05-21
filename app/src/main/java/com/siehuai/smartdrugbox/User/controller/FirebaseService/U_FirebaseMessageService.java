a32dc56b33d62ceecb1186fd5f7400fcda49e429
siehuai <gans0012@e.ntu.edu.sg>
2017-05-21 21:23:34 +0800

    Ureca 2017: Getting data from the notification

--- app/src/main/java/com/siehuai/smartdrugbox/User/controller/FirebaseService/U_FirebaseMessageService.java
+++ app/src/main/java/com/siehuai/smartdrugbox/User/controller/FirebaseService/U_FirebaseMessageService.java
@@ -4,11 +4,20 @@ import android.app.PendingIntent;
 import android.content.Intent;
 import android.util.Log;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+import com.siehuai.smartdrugbox.Generic.controller.IDbOnDataChangeListener;
 import com.siehuai.smartdrugbox.Generic.controller.Service.NotificationService;
 import com.siehuai.smartdrugbox.Generic.view.MainActivity;
 import com.siehuai.smartdrugbox.R;
+import com.siehuai.smartdrugbox.User.controller.RemoteDatabaseHelper.MedicineBoxCompartmentRemoteHelper;
+import com.siehuai.smartdrugbox.User.data.CompartmentDetails;
+import com.siehuai.smartdrugbox.User.data.MedicineBoxCompartment;
+import com.siehuai.smartdrugbox.User.data.MedicineDetails;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class U_FirebaseMessageService extends FirebaseMessagingService {
 
@@ -17,9 +26,19 @@ public class U_FirebaseMessageService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
+        String id = remoteMessage.getData().get("id");
+        String medicineBoxId = remoteMessage.getData().get("medicineBoxId");
+        MedicineBoxCompartmentRemoteHelper remoteHelper = new MedicineBoxCompartmentRemoteHelper();
+        remoteHelper.find(medicineBoxId, new IDbOnDataChangeListener() {
+            @Override
+            public void onDataChange(Object data) {
+                MedicineBoxCompartment medicineBoxCompartment = (MedicineBoxCompartment) data;
+                Log.d("Firebase Message",medicineBoxCompartment.toString());
+            }
+        });
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        createNotification(intent, remoteMessage.getNotification().getBody());
+        createNotification(intent, remoteMessage);
         Log.d("Firebase Message", remoteMessage.getNotification().getBody());
         mNotificationService.dispatchNotification();
     }
@@ -34,9 +53,11 @@ public class U_FirebaseMessageService extends FirebaseMessagingService {
         super.handleIntent(intent);
     }
 
-    private void createNotification(Intent intent, String message) {
+    private void createNotification(Intent intent, RemoteMessage message) {
         PendingIntent mPendingIntent = PendingIntent.getActivities(this, 0, new Intent[]{intent}, PendingIntent.FLAG_ONE_SHOT);
         mNotificationService = new NotificationService(this);
-        mNotificationService.createNotification("Firebase message", message, mPendingIntent, true, R.drawable.camera);
+        String title = message.getNotification().getTitle();
+        String content = message.getNotification().getBody();
+        mNotificationService.createNotification(title, content, mPendingIntent, true, R.drawable.camera);
     }
 }
