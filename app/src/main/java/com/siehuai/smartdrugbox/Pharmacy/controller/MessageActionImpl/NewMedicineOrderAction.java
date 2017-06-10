d048b76afa27e09040503266198ee86c13c56694
siehuai <edricgan.44@gmail.com>
2017-06-10 17:05:38 +0800

    Ureca 2017: Create notification when new medicine order is created

--- app/src/main/java/com/siehuai/smartdrugbox/Pharmacy/controller/MessageActionImpl/NewMedicineOrderAction.java
+++ app/src/main/java/com/siehuai/smartdrugbox/Pharmacy/controller/MessageActionImpl/NewMedicineOrderAction.java
@@ -1,9 +1,14 @@
 package com.siehuai.smartdrugbox.Pharmacy.controller.MessageActionImpl;
 
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
 
 import com.siehuai.smartdrugbox.Generic.controller.MessageAction.AbstractMessageAction;
 import com.siehuai.smartdrugbox.Generic.controller.Service.NotificationService;
+import com.siehuai.smartdrugbox.Pharmacy.view.P_ViewMedicineOrder.P_ViewMedicineOrder;
+import com.siehuai.smartdrugbox.R;
 
 import java.util.Map;
 
@@ -22,6 +27,19 @@ public class NewMedicineOrderAction extends AbstractMessageAction {
 
     @Override
     public void execute(Map<String, String> data) {
+        Intent intent = new Intent(mContext, P_ViewMedicineOrder.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        createNotification(intent);
+        Log.d("Firebase Message", getMessageBody());
+        mNotificationService.dispatchNotification();
+    }
 
+    private void createNotification(Intent intent) {
+        PendingIntent mPendingIntent = PendingIntent.getActivities(mContext, 0, new Intent[]{intent}, PendingIntent.FLAG_ONE_SHOT);
+        mNotificationService = new NotificationService(mContext);
+        String title = getMessageTitle();
+        String content = getMessageBody();
+        mNotificationService.createNotification(title, content, mPendingIntent, true, R.drawable.purchase_order);
+        mNotificationService.dispatchNotification();
     }
 }
