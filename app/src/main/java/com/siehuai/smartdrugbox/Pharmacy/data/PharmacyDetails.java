42591f7b0458bd8df214aee881ce5863d0bedeba
siehuai <edricgan.44@gmail.com>
2017-06-12 11:10:03 +0800

    Ureca 2017: Implement Parcelable interface in Pharmacy Details

--- app/src/main/java/com/siehuai/smartdrugbox/Pharmacy/data/PharmacyDetails.java
+++ app/src/main/java/com/siehuai/smartdrugbox/Pharmacy/data/PharmacyDetails.java
@@ -1,18 +1,21 @@
 package com.siehuai.smartdrugbox.Pharmacy.data;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.siehuai.smartdrugbox.Generic.data.IDbData;
 
-public class PharmacyDetails implements IDbData {
+public class PharmacyDetails implements IDbData, Parcelable {
 
-    String id;
-    String pharmacyImg;
-    String pharmacyName;
-    String ownerName;
-    String address;
-    String openingTime;
-    String closingTime;
-    String contactNumber;
-    String email;
+    private String id;
+    private String pharmacyImg;
+    private String pharmacyName;
+    private String ownerName;
+    private String address;
+    private String openingTime;
+    private String closingTime;
+    private String contactNumber;
+    private String email;
 
     @Override
     public void setId(String id) {
@@ -86,5 +89,47 @@ public class PharmacyDetails implements IDbData {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    private PharmacyDetails(Parcel in) {
+        id = in.readString();
+        pharmacyImg = in.readString();
+        pharmacyName = in.readString();
+        ownerName = in.readString();
+        address = in.readString();
+        openingTime = in.readString();
+        closingTime = in.readString();
+        contactNumber = in.readString();
+        email = in.readString();
+    }
+
+    public static final Parcelable.Creator<PharmacyDetails> CREATOR = new Parcelable.Creator<PharmacyDetails>() {
+        @Override
+        public PharmacyDetails createFromParcel(Parcel in) {
+            return new PharmacyDetails(in);
+        }
+
+        @Override
+        public PharmacyDetails[] newArray(int size) {
+            return new PharmacyDetails[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(id);
+        dest.writeString(pharmacyImg);
+        dest.writeString(pharmacyName);
+        dest.writeString(ownerName);
+        dest.writeString(address);
+        dest.writeString(openingTime);
+        dest.writeString(closingTime);
+        dest.writeString(contactNumber);
+        dest.writeString(email);
     }
 }
