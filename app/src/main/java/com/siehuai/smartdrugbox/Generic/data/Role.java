eb2f74e47061194efecd8f59f69075db8e69d6d3
siehuai <edricgan.44@gmail.com>
2017-06-20 00:45:55 +0800

    Ureca 2017: Put in default username for pharmacy and user
    1. We are using pharmacy role and user role in the same application and this cause null pointer
         if we didn't set default user name

--- app/src/main/java/com/siehuai/smartdrugbox/Generic/data/Role.java
+++ app/src/main/java/com/siehuai/smartdrugbox/Generic/data/Role.java
@@ -1,8 +1,8 @@
 package com.siehuai.smartdrugbox.Generic.data;
 
 public class Role {
-    public static String PATIENT_USERNAME;
+    public static String PATIENT_USERNAME = "Sie Huai";
     public static String DEFAULT_PATIENT_USERNAME = "Sie Huai";
     public static String DEFAULT_PHARMACY_USERNAME = "drugstore2";
-    public static String PHARMACY_USERNAME;
+    public static String PHARMACY_USERNAME = "drugstore2";
 }
