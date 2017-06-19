ed5154947c82d1f8efca30ad56601ac390953609
siehuai <edricgan.44@gmail.com>
2017-06-19 13:09:21 +0800

    Ureca 2017: Read the user name from the role class

--- app/src/main/java/com/siehuai/smartdrugbox/Pharmacy/controller/RemoteDatabaseHelper/PharmacyRemoteDbHelper.java
+++ app/src/main/java/com/siehuai/smartdrugbox/Pharmacy/controller/RemoteDatabaseHelper/PharmacyRemoteDbHelper.java
@@ -2,11 +2,12 @@ package com.siehuai.smartdrugbox.Pharmacy.controller.RemoteDatabaseHelper;
 
 import com.google.firebase.database.DatabaseReference;
 import com.siehuai.smartdrugbox.Generic.controller.RemoteDatabaseHelper.RemoteDbHelper;
+import com.siehuai.smartdrugbox.Generic.data.Role;
 
 public abstract class PharmacyRemoteDbHelper extends RemoteDbHelper {
     @Override
     protected DatabaseReference getDatabaseObj() {
         //TODO:Hardcoded username for now
-        return super.getDatabaseObj().child("Pharmacy/drugstore2");
+        return super.getDatabaseObj().child("Pharmacy").child(Role.PHARMACY_USERNAME);
     }
 }
--- app/src/main/java/com/siehuai/smartdrugbox/User/controller/RemoteDatabaseHelper/UserRemoteDbHelper.java
+++ app/src/main/java/com/siehuai/smartdrugbox/User/controller/RemoteDatabaseHelper/UserRemoteDbHelper.java
@@ -2,12 +2,13 @@ package com.siehuai.smartdrugbox.User.controller.RemoteDatabaseHelper;
 
 import com.google.firebase.database.DatabaseReference;
 import com.siehuai.smartdrugbox.Generic.controller.RemoteDatabaseHelper.RemoteDbHelper;
+import com.siehuai.smartdrugbox.Generic.data.Role;
 
 public abstract class UserRemoteDbHelper extends RemoteDbHelper {
 
     @Override
     protected DatabaseReference getDatabaseObj() {
         //TODO:Hardcoded username for now
-        return super.getDatabaseObj().child("User/Sie Huai");
+        return super.getDatabaseObj().child("User").child(Role.PATIENT_USERNAME);
     }
 }
