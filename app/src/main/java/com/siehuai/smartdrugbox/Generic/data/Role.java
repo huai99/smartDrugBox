d83a68c120804b0ba4bf1fb0d9915b37cb47ebb3
siehuai <edricgan.44@gmail.com>
2017-06-19 12:50:40 +0800

    Ureca 2017: Change the layout of the main login page
    1. Provide them the option to login with user name

---
+++ app/src/main/java/com/siehuai/smartdrugbox/Generic/data/Role.java
@@ -0,0 +1,8 @@
+package com.siehuai.smartdrugbox.Generic.data;
+
+public class Role {
+    public static String PATIENT_USERNAME;
+    public static String DEFAULT_PATIENT_USERNAME = "Sie Huai";
+    public static String DEFAULT_PHARMACY_USERNAME = "drugstore2";
+    public static String PHARMACY_USERNAME;
+}
--- app/src/main/java/com/siehuai/smartdrugbox/Generic/view/MainActivity.java
+++ app/src/main/java/com/siehuai/smartdrugbox/Generic/view/MainActivity.java
@@ -1,10 +1,16 @@
 package com.siehuai.smartdrugbox.Generic.view;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.annotation.IdRes;
 import android.view.View;
+import android.widget.RadioGroup;
 
+import com.siehuai.smartdrugbox.Generic.controller.Service.AlertDialogService;
+import com.siehuai.smartdrugbox.Generic.controller.Service.AlertDialogServiceFactory;
+import com.siehuai.smartdrugbox.Generic.data.Role;
 import com.siehuai.smartdrugbox.Pharmacy.controller.SubscribeToEventHelper;
 import com.siehuai.smartdrugbox.Pharmacy.view.P_MainActivity;
 import com.siehuai.smartdrugbox.R;
@@ -14,36 +20,98 @@ import com.siehuai.smartdrugbox.databinding.ActivityMainBinding;
 public class MainActivity extends BaseActivity {
 
     ActivityMainBinding mBinding;
+    AlertDialogService mAlertDialogService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mAlertDialogService = AlertDialogServiceFactory.createAlertDialogService(this);
         SubscribeToEventHelper.unSubscribeTopic("medicineOrder");
-        setContentView(R.layout.activity_main);
         mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
-        setUserBtnOnClick();
-        setPharmacyBtnOnClick();
+        setUpRadioBtn();
+        setUpLoginBtn();
     }
 
-    private void setUserBtnOnClick() {
-        mBinding.btnLoginUser.setOnClickListener(new View.OnClickListener() {
+    private void setUpEditText() {
+        if (mBinding.radioBtnUser.isChecked()) {
+            mBinding.editTextUserName.setText(Role.DEFAULT_PATIENT_USERNAME);
+        } else {
+            mBinding.editTextUserName.setText(Role.DEFAULT_PHARMACY_USERNAME);
+        }
+    }
+
+    private void setUpRadioBtn() {
+        mBinding.radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, U_MainActivity.class);
-                startActivity(intent);
+            public void onCheckedChanged(RadioGroup group, @IdRes int checkedId) {
+                setUpEditText();
             }
         });
     }
 
+    private void signInAsUser() {
+        if (mBinding.editTextUserName.getText() == null) {
+            promptNoUserNameErrorDialog();
+        } else {
+            String userName = String.valueOf(mBinding.editTextUserName.getText());
+            Role.PATIENT_USERNAME = userName;
+            Intent intent = new Intent(MainActivity.this, U_MainActivity.class);
+            startActivity(intent);
+        }
+    }
 
-    private void setPharmacyBtnOnClick() {
-        mBinding.btnLoginPharmacy.setOnClickListener(new View.OnClickListener() {
+    private void signInAsPharmacy() {
+        if (mBinding.editTextUserName.getText() == null) {
+            promptNoUserNameErrorDialog();
+        } else {
+            String userName = String.valueOf(mBinding.editTextUserName.getText());
+            Role.PHARMACY_USERNAME = userName;
+            Intent intent = new Intent(MainActivity.this, P_MainActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    private void setUpLoginBtn() {
+        mBinding.btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, P_MainActivity.class);
-                startActivity(intent);
+                if (mBinding.radioBtnUser.isChecked()) {
+                    signInAsUser();
+                } else {
+                    signInAsPharmacy();
+                }
             }
         });
     }
+
+    private void promptNoUserNameErrorDialog() {
+        mAlertDialogService.provideDefaultErrorDialog("Please key in your user name", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+    }
+
+//    private void setUserBtnOnClick() {
+//        mBinding.btnLoginUser.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, U_MainActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//    }
+//
+//
+//    private void setPharmacyBtnOnClick() {
+//        mBinding.btnLoginPharmacy.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(MainActivity.this, P_MainActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//    }
 
 }
