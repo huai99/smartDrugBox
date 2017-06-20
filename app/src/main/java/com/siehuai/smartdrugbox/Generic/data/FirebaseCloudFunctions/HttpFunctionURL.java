24127df9905f89bcc043e169030096b45a0b955c
siehuai <edricgan.44@gmail.com>
2017-06-20 23:36:18 +0800

    URECA 2017: Change the function url to the correct url

--- app/src/main/java/com/siehuai/smartdrugbox/Generic/data/FirebaseCloudFunctions/HttpFunctionURL.java
+++ app/src/main/java/com/siehuai/smartdrugbox/Generic/data/FirebaseCloudFunctions/HttpFunctionURL.java
@@ -1,5 +1,5 @@
 package com.siehuai.smartdrugbox.Generic.data.FirebaseCloudFunctions;
 
 public class HttpFunctionURL {
-    public static String GET_PHARMACY_WITH_MEDICINE = "https://us-central1-smartdrugbox-57a64.cloudfunctions.net/getPharmacyNameWithParticularMedicine";
+    public static String GET_PHARMACY_WITH_MEDICINE = "https://us-central1-smartdrugbox-6be17.cloudfunctions.net/getPharmacyNameWithParticularMedicine";
 }
